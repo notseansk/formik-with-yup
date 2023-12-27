05be316b7ad5bb943ece4e8b0a13b4694f638499
Shaun Pant <crizone.shaun@gmail.com>
2023-12-27 13:17:35 +0545

    added scroll y to admin user component

--- src/pages/RegisteredUsers.tsx
+++ src/pages/RegisteredUsers.tsx
@@ -21,12 +21,12 @@ const RegisteredUsers = () => {
     getUsers();
   }, []);
   return (
-    <section className="bg-[#1b1b1bc9] flex-grow p-6 ">
+    <section className="bg-[#1b1b1bc9] h-screen flex flex-col flex-grow p-6 ">
       <h2 className="text-center font-semibold text-4xl mb-8">
         List of Registered Users
       </h2>
-      <div className="bg-[#313131c9] rounded-xl w-full min-h-[90%] p-8">
-        <div className="rounded-lg bg-[#1b1b1b95] p-3 ">
+      <div className="bg-[#313131c9] h-[90%] rounded-xl w-full  p-8 ">
+        <div className="rounded-lg bg-[#1b1b1b95] p-3 h-[100%] flex flex-col  ">
           <div className="grid grid-cols-5 p-3 border-b-2 border-sky-600 ">
             <p className="text-lg">User ID</p>
             <p className="text-lg">Users</p>
@@ -34,22 +34,24 @@ const RegisteredUsers = () => {
             <p className="text-lg">Password</p>
             <button>Action</button>
           </div>
-          {users ? (
-            users.map((oneUserDetail) => (
-              <AdminUserComponent
-                key={oneUserDetail.id}
-                name={oneUserDetail.name}
-                email={oneUserDetail.email}
-                id={oneUserDetail.id}
-                password={oneUserDetail.password}
-                getUsers={getUsers}
-              />
-            ))
-          ) : (
-            <h5 className="text-3xl text-sky-400 text-center my-8">
-              fetching users please wait...
-            </h5>
-          )}
+          <div className="overflow-y-scroll">
+            {users ? (
+              users.map((oneUserDetail) => (
+                <AdminUserComponent
+                  key={oneUserDetail.id}
+                  name={oneUserDetail.name}
+                  email={oneUserDetail.email}
+                  id={oneUserDetail.id}
+                  password={oneUserDetail.password}
+                  getUsers={getUsers}
+                />
+              ))
+            ) : (
+              <h5 className="text-3xl text-sky-400 text-center my-8">
+                fetching users please wait...
+              </h5>
+            )}
+          </div>
         </div>
       </div>
     </section>
